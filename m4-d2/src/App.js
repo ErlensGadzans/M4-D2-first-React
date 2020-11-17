0b0ea6c9f96e591bfe6ee990fa9f4878f930523a
ErlensGadzans <erlens.gadzans@users.noreply.github.com>
2020-11-17 17:42:43 +0200

    Homework committing

--- m4-d2/src/App.js
+++ m4-d2/src/App.js
@@ -1,6 +1,12 @@
+import React from "react";
+import NavBar from "./components/NavBar"
+import Home from "./components/Home"
+
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
   return (
